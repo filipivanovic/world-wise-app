0352042952a875380d668d85ff58baf31dc2896e
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-06 23:02:40 +0200

    Nest routes under `AppLayout`, enhance `AppNav`, and update `Sidebar`
    - Updated the `App` component to nest routes (`cities`, `countries`, `form`) under the `AppLayout` route using `Outlet`, enabling better structuring of child pages.
    - Enhanced `AppNav` to include navigation links for `cities` and `countries` using `NavLink` for improved navigation experience.
    - Updated `Sidebar` to replace the static placeholder with `Outlet`, dynamically rendering nested routes under the `AppLayout`.
    - Improved the overall navigation and routing system to handle nested views effectively.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,12 @@ const App = () => {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
