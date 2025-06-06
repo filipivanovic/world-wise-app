63ab6ff3c667557d332a1c13a65f7732fad51161
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-06 23:39:04 +0200

    Fetch and display city data, update components and ESLint config
    - Implemented `useEffect` in `App.jsx` to fetch city data from `http://localhost:8000/cities` and display it dynamically.
    - Added states (`cities`, `isLoading`) to `App.jsx` for managing city data and loading state during fetch operation.
    - Updated `CityList` component to conditionally render a spinner, message, or a list of cities using the new `CityItem` component.
    - Created `CityItem` component to display individual city information with proper formatting and styling.
    - Modified `cities.json` to update emoji values and standardize encoding formats.
    - Disabled the `react/prop-types` rule in `.eslintrc.cjs` and reformatted rules for readability.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,30 @@ import AppLayout from './pages/AppLayout.jsx'
 import Login from './pages/Login.jsx'
 import CityList from './components/CityList.jsx'
 import CountryList from './components/CountryList.jsx'
+import { useEffect, useState } from 'react'
+
+const BASE_URL = 'http://localhost:8000'
 
 const App = () => {
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true)
+        const response = await fetch(`${BASE_URL}/cities`)
+        const data = await response.json()
+        setCities(data)
+      } catch (error) {
+        console.error(`Error in fetchCities method: ${error.message || error}`)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
@@ -20,7 +42,7 @@ const App = () => {
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<p>LIST</p>} />
-          <Route path="cities" element={<CityList />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="countries" element={<CountryList />} />
           <Route path="form" element={<p>form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,18 @@
 import styles from './CityList.module.css'
+import Spinner from './Spinner.jsx'
+import CityItem from './CityItem.jsx'
+import Message from './Message.jsx'
 
-const CityList = () => {
-  return <ul className={styles.cityList}>CITIES</ul>
+const CityList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />
+  if (!cities.length) return <Message message="No cities found" />
+  return (
+    <ul className={styles.cityList}>
+      {cities.map(city => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  )
 }
 
 export default CityList
