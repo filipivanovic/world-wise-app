0dc5a1f01168f72ccc64b0c8fdef90e8d5581999
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-06 21:40:36 +0200

    Add `Login` page component, routing, and update navigation link
    - Imported and added the `Login` page to the App component, creating a new route at `/login`.
    - Updated `PageNav` to include the `Login` link styled as a call-to-action.
    - Ensured consistent navigation experience with the addition of the `Login` page and link.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage.jsx'
 import Pricing from './pages/Pricing.jsx'
 import PageNotFound from './pages/PageNotFound.jsx'
 import AppLayout from './pages/AppLayout.jsx'
+import Login from './pages/Login.jsx'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />} />
       </Routes>
