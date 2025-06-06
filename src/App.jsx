7f2c9d890b294dc8b12ef7431f8906939ad13be3
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-06 19:47:05 +0200

    Integrate React Router and add routing for HomePage, Product, Pricing, and 404 page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Product from './pages/Product.jsx'
 import HomePage from './pages/HomePage.jsx'
 import Pricing from './pages/Pricing.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <Product />
-      <HomePage />
-      <Pricing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
