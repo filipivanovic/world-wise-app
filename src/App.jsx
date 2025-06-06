b3798884db85d46caf45dc82bf960e81bfd1ad10
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-06 19:37:49 +0200

    Add Product, HomePage, and Pricing components to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Product from './pages/Product.jsx'
+import HomePage from './pages/HomePage.jsx'
+import Pricing from './pages/Pricing.jsx'
+
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Product />
+      <HomePage />
+      <Pricing />
     </div>
   )
 }
