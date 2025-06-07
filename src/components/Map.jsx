dc86f92f6d472f2e6234c4af3ab9631a943badb1
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-07 18:39:01 +0200

    Add reusable `Button` component and update `Form` and `Map` components
    - Created a new `Button` component in `src/components/Button.jsx` with styling and props for `type`, `onClick`, and `children`, enabling customizable button usage across the app.
    - Refactored the `Form` component to replace standard `<button>` elements with the `Button` component for improved code reusability and styling consistency.
    - Integrated `useNavigate` in `Form` for handling navigation, allowing the "Back" button to dynamically go to the previous page.
    - Updated the `Map` component to navigate to the `Form` route on click, improving interactivity.
    - Standardized code formatting across components for improved readability and maintainability.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,20 @@
 import styles from './Map.module.css'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const Map = () => {
   const [searchParam, setSearchParam] = useSearchParams()
+  const navigate = useNavigate()
+
   const lat = searchParam.get('lat')
   const lng = searchParam.get('lng')
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      onClick={() => {
+        navigate('form')
+      }}
+      className={styles.mapContainer}
+    >
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
