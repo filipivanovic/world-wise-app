bf350db2d1334c03b073a0545eacdf73737a5066
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-08 20:25:40 +0200

    Highlight selected city in `CityItem` and enhance context integration
    - Added `useCities` context to the `CityItem` component to access the current city (`currentCity`) state.
    - Updated `CityItem` to apply an active styling class (`cityItem--active`) when the city ID matches the current city ID from the context.
    - Improved user experience by visually indicating the selected city in the list.
    -

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import styles from './CityItem.module.css'
 import { Link } from 'react-router-dom'
+import { useCities } from '../context/CitiesContext.jsx'
 
 const formatDate = date =>
   new Intl.DateTimeFormat('en', {
@@ -10,11 +11,15 @@ const formatDate = date =>
   }).format(new Date(date))
 
 const CityItem = ({ city }) => {
+  const { currentCity } = useCities()
   const { cityName, emoji, date, id, position } = city
 
   return (
     <li>
-      <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ''}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
