c1bb8a5c1fa05e35f6b8a4a28dadcbe013b7f658
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-09 22:44:46 +0200

    Integrate React Leaflet with basic map display in `Map` component
    - Added `react-leaflet` components (`MapContainer`, `Marker`, `Popup`, and `TileLayer`) to the `Map` component for embedding maps.
    - Updated `Map` to render an interactive map with a fixed initial position ([40, 0]) and zoom level 13.
    - Integrated `Popup` to display a customizable message and `Marker` for pinpointing location on the map.
    - Replaced placeholder content with functional Leaflet map elements for improved functionality and user experience.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,11 @@
 import styles from './Map.module.css'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useState } from 'react'
 
 const Map = () => {
   const [searchParam, setSearchParam] = useSearchParams()
+  const [mapPosition, setMapPosition] = useState([40, 0])
   const navigate = useNavigate()
 
   const lat = searchParam.get('lat')
@@ -15,11 +18,22 @@ const Map = () => {
       }}
       className={styles.mapContainer}
     >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParam({ lat: 23, lng: 50 })}>Change position</button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.mapContainer}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
