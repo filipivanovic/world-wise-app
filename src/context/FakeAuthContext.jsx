f0d5bf749f0a40c31d29f43e251f92dca2a88c39
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-13 23:39:10 +0200

    Add `FakeAuthContext` with `AuthProvider` and `useAuth` hook
    - Created a new `FakeAuthContext` in `src/context/FakeAuthContext.jsx` to manage authentication state.
    - Added an `AuthProvider` component to wrap children with authentication context, currently providing a static `true` value.
    - Implemented a custom `useAuth` hook to access the authentication context and enforce usage within the `AuthProvider`.
    - Throws an error if `useAuth` is called outside of an `AuthProvider`, ensuring proper context usage.

---
+++ src/context/FakeAuthContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from 'react'
+
+const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext.Provider value={true}>{children}</AuthContext.Provider>
+}
+
+const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a AuthProvider')
+  }
+}
