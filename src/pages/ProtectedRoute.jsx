a3e3a93d44315dacf2faafeef125453d5b55afbc
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-14 18:30:41 +0200

    Add `ProtectedRoute` component for route guarding
    - Created a new `ProtectedRoute` component in `src/pages/ProtectedRoute.jsx` to secure specific routes.
    - Utilized `useAuth` to check the authentication status (`isAuthenticated`) of users.
    - Implemented a `useEffect` with `navigate` from `react-router-dom` to redirect unauthenticated users to the homepage (`/`).
    - Designed the component to render its children only when the user is authenticated, ensuring proper route protection.

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useAuth } from '../context/FakeAuthContext.jsx'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/')
+    }
+  }, [isAuthenticated, navigate])
+  return children
+}
+
+export default ProtectedRoute
