0a168e93bfee2e8b55466be3c19168540b3b6399
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-14 18:07:28 +0200

    Wrap `App` with `AuthProvider` for authentication context
    - Nested the `CitiesProvider` and `BrowserRouter` components within the `AuthProvider` in `App.jsx` to introduce authentication context to the application.
    - Enables authentication state management across the application, preparing for secure and authenticated route handling.

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,30 @@ import CountryList from './components/CountryList.jsx'
 import City from './components/City.jsx'
 import Form from './components/Form.jsx'
 import { CitiesProvider } from './context/CitiesContext.jsx'
+import { AuthProvider } from './context/FakeAuthContext.jsx'
 
 const App = () => {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate to="cities" replace />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   )
 }
 
